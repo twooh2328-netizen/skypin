9d9a321320044b141be53369c180f19af9550ed4
twooh2328-netizen <twooh2328@gmail.com>
2026-04-10 19:36:06 +0900

    Add images to beach location entries
    Added image URLs for each beach location.

--- sports.js
+++ sports.js
@@ -1,26 +1,30 @@
 const spots = [
-  {
-    name: "양양 서피비치",
-    lat: 38.0295,
-    lng: 128.7319,
-    description: "서핑과 일출 드론 촬영 명소"
-  },
-  {
-    name: "대부도 방아머리 해변",
-    lat: 37.2526,
-    lng: 126.5822,
-    description: "갯벌과 노을 촬영 포인트"
-  },
-  {
-    name: "제부도 바닷길",
-    lat: 37.1666,
-    lng: 126.6184,
-    description: "바닷길 열리는 장면 촬영 가능"
-  },
-  {
-    name: "굴업도 개머리언덕",
-    lat: 37.2308,
-    lng: 126.0957,
-    description: "초원과 바다 절경 드론 촬영지"
-  }
+{
+name:"양양 서피비치",
+lat:38.0295,
+lng:128.7319,
+description:"서핑과 일출 드론 촬영 명소",
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Yangyang_Surfyy_Beach.jpg/640px-Yangyang_Surfyy_Beach.jpg"
+},
+{
+name:"대부도 방아머리 해변",
+lat:37.2526,
+lng:126.5822,
+description:"갯벌과 노을 촬영 포인트",
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Daebudo_Beach.jpg/640px-Daebudo_Beach.jpg"
+},
+{
+name:"제부도 바닷길",
+lat:37.1666,
+lng:126.6184,
+description:"바닷길 열리는 장면 촬영 가능",
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Jebudo_Island_Road.jpg/640px-Jebudo_Island_Road.jpg"
+},
+{
+name:"굴업도 개머리언덕",
+lat:37.2308,
+lng:126.0957,
+description:"초원과 바다 절경 드론 촬영지",
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Guleopdo_Island.jpg/640px-Guleopdo_Island.jpg"
+}
 ];
