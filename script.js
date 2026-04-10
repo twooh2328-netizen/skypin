146dfefa4bb0b106b47b756b0d33c11455715d15
twooh2328-netizen <twooh2328@gmail.com>
2026-04-10 19:37:53 +0900

    Use custom drone icon for map markers
    Updated marker to use custom drone icon for spots.

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
 maxZoom:19
 }).addTo(map);
 
+var droneIcon = L.icon({
+iconUrl:"https://cdn-icons-png.flaticon.com/512/744/744465.png",
+iconSize:[32,32]
+});
+
 spots.forEach(function(spot){
 
-var marker = L.marker([spot.lat,spot.lng]).addTo(map);
+var marker = L.marker([spot.lat,spot.lng],{icon:droneIcon}).addTo(map);
 
 marker.bindPopup(
-"<b>"+spot.name+"</b><br>"+spot.description
+"<h3>"+spot.name+"</h3>"+
+"<img src='"+spot.image+"' width='220'><br>"+
+spot.description
 );
 
 });
