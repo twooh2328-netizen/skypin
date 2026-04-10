80e27220a439266217c7b0166bd7a47f675618ac
twooh2328-netizen <twooh2328@gmail.com>
2026-04-10 20:54:58 +0900

    Refactor map and marker handling in script.js

--- script.js
+++ script.js
@@ -1,158 +1,168 @@
 var map=L.map('map').setView([36.5,127.8],7);
 
-L.tileLayer(
+var normal=L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {maxZoom:19}
 ).addTo(map);
 
+var satellite=L.tileLayer(
+'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+);
+
 var droneIcon=L.icon({
-iconUrl:"https://cdn-icons-png.flaticon.com/512/744/744465.png",
+
+iconUrl:"https://cdn-icons-png.flaticon.com/512/854/854878.png",
+
 iconSize:[32,32]
+
 });
 
 var markers=[];
 
-function addSpot(spot){
+function drawSpots(list){
+
+markers.forEach(function(m){
+
+map.removeLayer(m);
+
+});
+
+markers=[];
+
+list.forEach(function(s){
 
 var marker=L.marker(
-[spot.lat,spot.lng],
+[s.lat,s.lng],
 {icon:droneIcon}
 ).addTo(map);
 
 marker.bindPopup(
 
-"<h3>"+spot.name+"</h3>"+
-"<img src='"+spot.image+"' width='220'><br>"+
-spot.description
+"<b>"+s.name+"</b><br>"+
+s.description+
+"<br><img src='"+s.image+"' width='200'>"
 
 );
 
-markers.push({
-data:spot,
-marker:marker
+markers.push(marker);
+
 });
 
 }
 
-spots.forEach(addSpot);
+drawSpots(spots);
+
 
 /* 검색 */
 
 document.getElementById("searchBox")
-.addEventListener("keyup",function(){
+
+.addEventListener("keyup",function(e){
+
+if(e.key!=="Enter")return;
 
 var keyword=this.value.toLowerCase();
 
-markers.forEach(function(item){
+var found=spots.find(function(s){
 
-if(item.data.name.toLowerCase().includes(keyword)){
-
-map.setView(item.marker.getLatLng(),10);
-item.marker.openPopup();
-
-}
-
-});
-
-});
-
-/* 필터 */
-
-document.getElementById("filter")
-.onchange=function(){
-
-var type=this.value;
-
-markers.forEach(function(item){
-
-if(type==="all" || item.data.type===type){
-
-map.addLayer(item.marker);
-
-}else{
-
-map.removeLayer(item.marker);
-
-}
-
-});
-
-};
-
-/* 지도 클릭 촬영지 추가 */
-
-map.on("click",function(e){
-
-var name=prompt("촬영지 이름");
-if(!name) return;
-
-var desc=prompt("촬영 설명");
-
-var spot={
-name:name,
-lat:e.latlng.lat,
-lng:e.latlng.lng,
-type:"beach",
-description:desc,
-image:"https://cdn-icons-png.flaticon.com/512/854/854878.png"
-};
-
-addSpot(spot);
-
-});
-
-/* 내 위치 */
-
-var myLocationMarker;
-
-document.getElementById("locBtn").onclick=function(){
-
-map.locate({setView:true,maxZoom:12});
-
-};
-
-map.on("locationfound",function(e){
-
-if(myLocationMarker){
-map.removeLayer(myLocationMarker);
-}
-
-myLocationMarker=L.marker(
-e.latlng,
-{icon:droneIcon}
-).addTo(map);
-
-myLocationMarker
-.bindPopup("📍 현재 위치")
-.openPopup();
-
-});
-/* 검색 기능 */
-
-document.getElementById("searchBox")
-.addEventListener("keyup", function(e){
-
-if(e.key !== "Enter") return;
-
-var keyword = this.value.toLowerCase();
-
-var found = spots.find(function(s){
 return s.name.toLowerCase().includes(keyword);
+
 });
 
 if(found){
 
 map.setView([found.lat,found.lng],13);
 
-L.popup()
-.setLatLng([found.lat,found.lng])
-.setContent("📍 "+found.name+"<br>"+found.description)
-.openOn(map);
+}
+
+});
+
+
+/* 필터 */
+
+document.getElementById("categoryFilter")
+
+.onchange=function(){
+
+var value=this.value;
+
+if(value==="all"){
+
+drawSpots(spots);
+
+return;
+
+}
+
+var filtered=spots.filter(function(s){
+
+return s.category===value;
+
+});
+
+drawSpots(filtered);
+
+};
+
+
+/* 위성지도 */
+
+var sat=false;
+
+document.getElementById("satBtn")
+
+.onclick=function(){
+
+if(!sat){
+
+map.removeLayer(normal);
+
+satellite.addTo(map);
+
+sat=true;
 
 }else{
 
-alert("촬영지를 찾을 수 없습니다.");
+map.removeLayer(satellite);
+
+normal.addTo(map);
+
+sat=false;
 
 }
+
+};
+
+
+/* 내 위치 */
+
+var myLocation;
+
+document.getElementById("locBtn")
+
+.onclick=function(){
+
+map.locate({setView:true,maxZoom:13});
+
+};
+
+map.on("locationfound",function(e){
+
+if(myLocation){
+
+map.removeLayer(myLocation);
+
+}
+
+myLocation=L.marker(
+e.latlng,
+{icon:droneIcon}
+)
+
+.addTo(map)
+
+.bindPopup("현재 위치")
+
+.openPopup();
 
 });
