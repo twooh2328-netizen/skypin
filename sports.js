b7732d7ef9a6a90e3192c88664dfa03e36dbfd8e
twooh2328-netizen <twooh2328@gmail.com>
2026-04-10 20:26:15 +0900

    Update beach and mountain spots in sports.js

--- sports.js
+++ sports.js
@@ -1,30 +1,48 @@
-const spots = [
+const spots=[
+
 {
 name:"양양 서피비치",
 lat:38.0295,
 lng:128.7319,
+type:"beach",
 description:"서핑과 일출 드론 촬영 명소",
 image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Yangyang_Surfyy_Beach.jpg/640px-Yangyang_Surfyy_Beach.jpg"
 },
+
 {
-name:"대부도 방아머리 해변",
+name:"하조대",
+lat:38.0277,
+lng:128.7157,
+type:"sunrise",
+description:"동해 일출 촬영 포인트",
+image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Hajodae.jpg/640px-Hajodae.jpg"
+},
+
+{
+name:"대부도 방아머리",
 lat:37.2526,
 lng:126.5822,
-description:"갯벌과 노을 촬영 포인트",
+type:"sunset",
+description:"노을 촬영 명소",
 image:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Daebudo_Beach.jpg/640px-Daebudo_Beach.jpg"
 },
+
 {
 name:"제부도 바닷길",
 lat:37.1666,
 lng:126.6184,
-description:"바닷길 열리는 장면 촬영 가능",
+type:"beach",
+description:"바닷길 열리는 장면 촬영",
 image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Jebudo_Island_Road.jpg/640px-Jebudo_Island_Road.jpg"
 },
+
 {
 name:"굴업도 개머리언덕",
 lat:37.2308,
 lng:126.0957,
-description:"초원과 바다 절경 드론 촬영지",
+type:"mountain",
+description:"초원 절경 드론 촬영지",
 image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Guleopdo_Island.jpg/640px-Guleopdo_Island.jpg"
 }
+
 ];
