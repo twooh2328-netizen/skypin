e1953bd1799314f2a2d2b38aa31ce141e4402b7a
twooh2328-netizen <twooh2328@gmail.com>
2026-04-10 20:53:57 +0900

    Refactor spots array to use 'var' and update entries

--- sports.js
+++ sports.js
@@ -1,48 +1,39 @@
-const spots=[
+var spots=[
 
 {
-name:"양양 서피비치",
-lat:38.0295,
-lng:128.7319,
-type:"beach",
-description:"서핑과 일출 드론 촬영 명소",
-image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Yangyang_Surfyy_Beach.jpg/640px-Yangyang_Surfyy_Beach.jpg"
+name:"성산일출봉",
+lat:33.458,
+lng:126.942,
+category:"sunrise",
+description:"제주 일출 촬영지",
+image:"https://upload.wikimedia.org/wikipedia/commons/3/3b/Seongsan_Ilchulbong.jpg"
 },
 
 {
-name:"하조대",
-lat:38.0277,
-lng:128.7157,
-type:"sunrise",
-description:"동해 일출 촬영 포인트",
-image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Hajodae.jpg/640px-Hajodae.jpg"
+name:"광안리",
+lat:35.153,
+lng:129.118,
+category:"night",
+description:"부산 야경 촬영지",
+image:"https://upload.wikimedia.org/wikipedia/commons/e/e4/Gwangan_Bridge.jpg"
 },
 
 {
-name:"대부도 방아머리",
-lat:37.2526,
-lng:126.5822,
-type:"sunset",
-description:"노을 촬영 명소",
-image:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Daebudo_Beach.jpg/640px-Daebudo_Beach.jpg"
+name:"양양 서핑비치",
+lat:38.061,
+lng:128.669,
+category:"beach",
+description:"서핑 드론 촬영",
+image:"https://upload.wikimedia.org/wikipedia/commons/5/5f/Yangyang_Surfyy_Beach.jpg"
 },
 
 {
-name:"제부도 바닷길",
-lat:37.1666,
-lng:126.6184,
-type:"beach",
-description:"바닷길 열리는 장면 촬영",
-image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Jebudo_Island_Road.jpg/640px-Jebudo_Island_Road.jpg"
-},
-
-{
-name:"굴업도 개머리언덕",
-lat:37.2308,
-lng:126.0957,
-type:"mountain",
-description:"초원 절경 드론 촬영지",
-image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Guleopdo_Island.jpg/640px-Guleopdo_Island.jpg"
+name:"설악산",
+lat:38.119,
+lng:128.465,
+category:"mountain",
+description:"산악 드론 촬영",
+image:"https://upload.wikimedia.org/wikipedia/commons/0/0c/Seoraksan.jpg"
 }
 
 ];
