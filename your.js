f5c36d148ade21ff8e37e9885a023e26d1981aa8
twooh2328-netizen <twooh2328@gmail.com>
2026-05-14 00:43:53 +0900

    Update Leaflet icon URLs in your.js
    Updated Leaflet icon URLs for markers.

--- your.js
+++ your.js
@@ -2,6 +2,18 @@ window.addEventListener("DOMContentLoaded", () => {
 
 console.log("SkyPin START");
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+
+  iconUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+
+  shadowUrl:
+    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+});   
 /* =========================
    추천명소
 ========================= */
